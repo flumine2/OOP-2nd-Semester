e888d5633e028b34ee94f34eabd9268a57873682
Savienkov Maksym <froggt25@gmail.com>
2022-06-13 15:59:33 +0300

    Added base command

--- Lab6/ViewModels/Base/ViewModel.cs
+++ Lab6/ViewModels/Base/ViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab6.ViewModels.Base
 {
-    internal abstract class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyCnaged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyCnaged(PropertyName);
+            return true;
+        }
     }
 }
---
+++ Lab6/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Lab6.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Lab 6";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
