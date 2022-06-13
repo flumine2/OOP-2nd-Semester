0b8041de85b984fe8076bb7189025a41de620229
Savienkov Maksym <froggt25@gmail.com>
2022-06-13 16:29:48 +0300

    Code clean up

--- Lab6/Infrastructure/Commands/LambdaCommand.cs
+++ Lab6/Infrastructure/Commands/LambdaCommand.cs
@@ -1,9 +1,5 @@
 ﻿using Lab6.Infrastructure.Commands.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab6.Infrastructure.Commands
 {
