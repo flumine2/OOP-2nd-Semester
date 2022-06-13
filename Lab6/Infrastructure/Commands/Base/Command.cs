0d33408f4015c89b7126310fd49a326583772e7d
Savienkov Maksym <froggt25@gmail.com>
2022-06-13 16:28:47 +0300

    Added lambda command

--- Lab6/Infrastructure/Commands/Base/Command.cs
+++ Lab6/Infrastructure/Commands/Base/Command.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Lab6.Infrastructure.Command.Base
+namespace Lab6.Infrastructure.Commands.Base
 {
     internal abstract class Command : ICommand
     {
